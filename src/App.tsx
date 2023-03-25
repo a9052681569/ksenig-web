0afe07319febd4249088f64bc363f9d0df049a54
a9052681569 <a9052681569@gmail.com>
2023-03-25 19:56:33 +0300

    переделал пидора на котика

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 import './App.scss';
+import catPic from './assets/cat.jpg';
 
 export const App: FC = () => (
 	<div className="App">
-		<h1>Ты пидор бля внатуре работает охуеть bleat</h1>
+		<h1>Ты котик</h1>
+		<img src={catPic} />
 	</div>
 );
